39ef11083c479bc18674522d6f699e1c5992de4c
oosawa-shingo <141128249+oosawa-shingo@users.noreply.github.com>
2024-06-06 14:27:24 +0000

    CSS操作

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-$(function(){
-  $('body').html('<h1>Hello jQuery!!</h1>');
+// $(function(){
+//   $('.box1').slideDown(1000);
+//   $('.box1').show();
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px',
+//     'width': '200px'
+//   });
+//   $('.box1').slideUp(1000);
+// });
+
+$(function (){
+  $('.box1').slideDown(5000,function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp(5000);
+  });
 });
