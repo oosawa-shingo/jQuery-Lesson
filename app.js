10860460b9db03505ca81dc46e00bf9056259492
oosawa-shingo <141128249+oosawa-shingo@users.noreply.github.com>
2024-06-09 18:50:01 +0000

    イベント

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-// $(function(){
-//   $('.box1').slideDown(1000);
-//   $('.box1').show();
-//   $('.box1').css({
-//     'background-color': '#0000FF',
-//     'height': '100px',
-//     'width': '200px'
-//   });
-//   $('.box1').slideUp(1000);
-// });
-
-$(function (){
-  $('.box1').slideDown(5000,function (){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp(5000);
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
